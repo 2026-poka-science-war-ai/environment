f4a59202b3d0200afc20f28b7e06958bb77d67c8
jae1u <yjw_0412@naver.com>
2026-07-05 01:05:48 +0900

    fix: rename scenario to `MarioKartWiiRace`

--- environment/scenario/services.py
+++ environment/scenario/services.py
@@ -16,13 +16,13 @@ _LOGGER = logging.getLogger(__name__)
 class MarioKartWiiRace(DolphinScenario):
     class Session(DolphinScenario.Session):
         def terminated(self) -> Terminated:
-            _LOGGER.debug("Checking Grand Prix terminated state")
-            # TODO: implement this method to determine if the Grand Prix scenario in Mario Kart Wii has terminated, e.g. by checking all the players have finished the race
+            _LOGGER.debug("Checking MarioKartWiiRace terminated state")
+            # TODO: implement this method to determine if the Mario Kart Wii race has terminated, e.g. by checking all the players have finished the race
             return Terminated(False)
 
         def truncated(self) -> Truncated:
-            _LOGGER.debug("Checking Grand Prix truncated state")
-            # TODO: implement this method to determine if the Grand Prix scenario in Mario Kart Wii has been truncated
+            _LOGGER.debug("Checking MarioKartWiiRace truncated state")
+            # TODO: implement this method to determine if the Mario Kart Wii race has been truncated
             return Truncated(False)
 
     def __init__(self, configuration: RaceConfiguration, dolphin: Dolphin):
@@ -38,9 +38,9 @@ class MarioKartWiiRace(DolphinScenario):
     def session(self) -> Iterator[MarioKartWiiRace.Session]:
         _LOGGER.info("Opening MarioKartWiiRace session")
         with self._dolphin.session() as dolphin_session:
-            # TODO: e.g. navigating the Dolphin emulator to the Grand Prix mode in Mario Kart Wii.
+            # TODO: e.g. navigating the Dolphin emulator to the race menus in Mario Kart Wii.
             scenario_session = MarioKartWiiRace.Session(dolphin_session=dolphin_session)
             _LOGGER.debug("Mario Kart scenario session ready")
             yield scenario_session
             _LOGGER.info("Closing MarioKartWiiRace session")
-            # TODO: e.g. cleaning up after the Grand Prix scenario in Mario Kart Wii.
+            # TODO: e.g. cleaning up after the Mario Kart Wii race.
